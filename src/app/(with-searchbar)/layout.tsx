449daca3ff20a1561add3a507d21a26e1bf6ef44
wondonghwi <ehdehd1289@naver.com>
2025-03-02 20:07:22 +0900

    feat: 클라이언트 라우터 캐시 적용테스트

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -4,6 +4,7 @@ import React, { Suspense } from 'react';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <div>{new Date().toLocaleString()}</div>
       <Suspense fallback={<div>Loading...</div>}>
         <SearchBar />
       </Suspense>
--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -2,6 +2,13 @@ import BookItem from '@/components/book-item';
 import style from './page.module.css';
 import { BookData } from '@/types';
 
+// NOTE: 페이지 동작 방식 설정
+// export const dynamic = 'auto';
+// 1. auto : 기본값 - 아무것도 강제하지않음
+// 2. force-dynamic : 모든 페이지를 동적으로 처리
+// 3. force-static : 모든 페이지를 정적으로 처리
+// 4. error : 페이지를 강제로 static 페이지 설정 ( 설정하면 안되는 이유 -> 빌드오류 )
+
 async function AllBooks() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
